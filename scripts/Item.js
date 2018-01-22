4e086ebfe6cf98da3c9dc8d9fd76da4cf1d8e77b
Adrian Ross <adriantoddross@gmail.com>
2018-01-22 12:45:34 -0500

    finished half of namespace

--- scripts/Item.js
+++ scripts/Item.js
@@ -3,9 +3,29 @@
 const item = (function () {
   const foo = 'bar';
 
-  return {};
+  return {
+    validateName(){},
+    create(){}
+  };
 
 }() );
 
+const validateName = function (name) {
+  // Makes sure we can't add items without a name.
+  return name ? ' ' : console.log('Name does not exist.');
+};
+
+const create = function (name) {
+  // The function should return a new item object with the following attributes:
+  // id - invoke cuid() to create a unique id
+  // name - the value of parameter name
+  // checked - defaults to false
+  return {
+    id: cuid(),
+    name: name,
+    checked: false
+  };
+};
+
 console.log(item);
-console.log(foo);
+// console.log(foo);
--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,24 @@
 
 // eslint-disable-next-line no-unused-vars
 
+'use strict';
+
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+// Testing file. 
+  // const itemNames = [ '', 'apples', 'pears' ];
+  // itemNames.forEach(name => {
+  //   try {
+  //     Item.validateName(name);
+  //     store.items.push(Item.create(name));
+  //   } catch(error) {
+  //     console.log('Cannot add item: ' + error.message);
+  //   }
+  // });
+  // shoppingList.render();
 });
 
 console.log(store);
-console.log(foo);
+// console.log(foo);
